b956a170a80f4ba9ac760e77f77239878a11536e
MackendyTechnology <bouquetmackendy34@gmail.com>
2023-10-09 18:58:18 -0400

    Exercice-JS-4

---
+++ index.js
@@ -0,0 +1,24 @@
+function square(num) {
+    return num ** 2;
+ }
+ console.log(square(5));
+ 
+ function raiseToCube(num) {
+    return num ** 3;
+ }
+ console.log(raiseToCube(3));
+ 
+ function raise(num, exponent) {
+    return num ** exponent;
+ }
+ console.log(raise(2, 4));
+ 
+ function roundNumber(num) {
+    return Math.round(num);
+ }
+ console.log(roundNumber(3.7));
+ 
+ function randomnumber() {
+    return Math.floor(Math.random() * 11);
+ }
+ console.log(randomnumber());
